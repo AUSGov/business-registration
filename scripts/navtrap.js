cec2ebe31f6b767158317dbcdd01479f1201dd20
dssindustry <david.shaw@industry.gov.au>
2018-02-02 13:53:14 +1100

    updates

--- scripts/navtrap.js
+++ scripts/navtrap.js
@@ -1,28 +1,32 @@
-var CatchBackButton = {
+﻿var CatchBackButton = {
     Init: function (prompt, action, watch) {
         // create an iframe to host navigation events
         this.iframe = createiframe();
-        
+		this.iframe.src = "#test";
+        this.iframe.contentWindow.postMessage("init", "*");
+		
+
         // use window.confirm if we weren't passed a prompt function
         this.prompt = prompt != null ? prompt : function (action) {
             if (window.confirm("Are you sure you want to leave?")) {
-                if (action != null)
-                    action();
-            }
+                action != null ? action() : history.back();
+            } else {
+				CatchBackButton.iframe.contentWindow.postMessage("update", "*");
+			}
         }
         // listen for back-button navigation message from the iframe
         window.addEventListener("message", function (event) {
             var key = event.message ? 'message' : 'data';
             var data = event[key];
             if (data === "nav-back") {
-                navtrap.prompt(navtrap.action);
-                navtrap.iframe.contentWindow.postMessage("update", "*");
+                CatchBackButton.prompt(CatchBackButton.action);
+                //CatchBackButton.iframe.contentWindow.postMessage("update", "*");
             }
         });
         // push iframe back to the top of the history stack on changes to 'watch'
-        if (listen !== null) {
-            $(listen).on("load", function () {
-                navtrap.iframe.contentWindow.postMessage("update", "*");
+        if (watch !== null) {
+            $(watch).on("load", function () {
+                CatchBackButton.iframe.contentWindow.postMessage("update", "*");
             });
         }
     },
@@ -40,18 +44,17 @@ function createiframe() {
     $iframe.contentDocument.open();
     $iframe.contentDocument.write("<html><head>" +
         "<script>" +
-        "window.addEventListener('onload', function (event) {" +
-        "history.pushState({ id: 'idle' }, '', 'idlepage');" +
-        "});" +
-        "window.addEventListener('popstate', function (event) {" +
-        "parent.postMessage('nav-back', '*');" +
-        "});" +
         "window.addEventListener('message', function (event) {" +
         "var key = event.message ? 'message' : 'data';" +
         "var data = event[key];" +
-        "if (data === 'update')" +
-        "history.pushState({id: 'idle' }, '', 'idlepage');" +
-        "}, true);" +
+		"switch (data) {" +
+		"case 'init': history.pushState({id: 'idle' }, '', '#');" +
+		"window.addEventListener('popstate', function (event) {" +
+        "parent.postMessage('nav-back', '*');" +
+        "});" +
+		"break;" +
+        "case 'update': history.pushState({id: 'idle' }, '', '#');" +
+		"}}, true);" +
         "</script>" +
         "</head ><body><p>TESTING</p></body></html>");
     $iframe.contentDocument.close();
